9d0b7754daaad49ed0b166ef62f42b9fbabc789b
Alina <dizialina@gmail.com>
2016-01-08 22:21:04 +0200

    first commit

---
+++ SQLite3DBSample/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  SQLite3DBSample
+//
+//  Created by Admin on 08.01.16.
+//  Copyright © 2016 Alina Egorova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ SQLite3DBSample/EditInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditInfoViewController.h
+//  SQLite3DBSample
+//
+//  Created by Admin on 08.01.16.
+//  Copyright © 2016 Alina Egorova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditInfoViewController : UIViewController
+
+@end
