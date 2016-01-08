22850b83c0c98c6e86f2e49798ffdd1eb006b757
Alina <dizialina@gmail.com>
2016-01-08 18:16:51 +0200

    Initial Commit

---
+++ SQLite3DBSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLite3DBSample
+//
+//  Created by Admin on 08.01.16.
+//  Copyright © 2016 Alina Egorova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SQLite3DBSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SQLite3DBSample
+//
+//  Created by Admin on 08.01.16.
+//  Copyright © 2016 Alina Egorova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
